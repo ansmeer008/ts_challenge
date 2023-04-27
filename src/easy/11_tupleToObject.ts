c3c1d2e9e7ec298164cb7d1c32a1c122a54b867a
Jade <neederjade@gmail.com>
2023-04-28 02:26:17 +0900

    주석 추가

--- src/easy/11_tupleToObject.ts
+++ src/easy/11_tupleToObject.ts
@@ -19,6 +19,12 @@
 */
 
 /* _____________ Your Code Here _____________ */
+//아래 테스트 케이스를 봤을 때 배열들은 number, string으로 구성되어 있고
+//const이기 때문에 수정이 불가능 하므로 ReadOnlyArray
+//결과인 객체는 key와 value 값이 동일하다
+//튜플 = 길이와 타입이 고정된 배열
+//배열의 각 인덱스마다의 타입이 고정되어 있다고 보면 됨
+//***push 매서드 사용하면 튜플 규칙 무시한다는 맹점 존재****/
 
 type TupleToObject<T extends ReadonlyArray<number | string>> = {
   [P in T[number]]: P;
