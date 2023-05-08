61ca0bb93cd7ecc1df2ec91c5811dbf9060657b1
Jade <neederjade@gmail.com>
2023-05-08 22:39:38 +0900

    898 includes

---
+++ src/easy/898_includes.ts
@@ -0,0 +1,62 @@
+/*
+  898 - Includes
+  -------
+  by null (@kynefuk) #easy #array
+
+  ### Question
+
+  Implement the JavaScript `Array.includes` function in the type system. A type takes the two arguments. The output should be a boolean `true` or `false`.
+
+  For example:
+
+  ```ts
+  type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
+  ```
+
+  > View on GitHub: https://tsch.js.org/898
+*/
+
+/* _____________ Your Code Here _____________ */
+
+//1. 처음 작성한 답 => 통과 X 
+type Includes<T extends readonly any[], U extends any> = U extends T[number]
+
+//2. Equal 활용하기 
+//**keyof 연산자는 객체 형태 타입을 따로 속성들만 뽑아 유니온 타입으로 만들어주는 연산자 
+//X는 T의 인덱스, 키 역할 
+extends flase[] 뒤에 조건부 연산자를 걸어줌 
+type Includes<T extends readonly any[],U> = (
+  {[X in keyof T]: Equal<T[X],U>}) extends false[] ? false : true
+
+
+//3. Equal 활용, infer 이용한 풀이 (조건부와 재귀를 이용해서 풀기)
+type Includes<T extends readonly any[],U> = T extends [infer F, ... infer O] ? (Equal<F,U> extends true ? true : Includes<O,U>) : false;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/898/answer
+  > View solutions: https://tsch.js.org/898/solutions
+  > More Challenges: https://tsch.js.org
+*/
