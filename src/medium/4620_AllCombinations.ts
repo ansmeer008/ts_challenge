efed672d21fd2087950208ea16bb0dc04db5617a
Jade <neederjade@gmail.com>
2023-08-01 13:11:19 +0900

    4620_AllCombinations

---
+++ src/medium/4620_AllCombinations.ts
@@ -0,0 +1,155 @@
+/*
+  4260 - AllCombinations
+  -------
+  by 蛭子屋双六 (@sugoroku-y) #medium #template-literal #infer #union
+
+  ### Question
+
+  Implement type ```AllCombinations<S>``` that return all combinations of strings which use characters from ```S``` at most once.
+
+  For example:
+
+  ```ts
+  type AllCombinations_ABC = AllCombinations<'ABC'>;
+  // should be '' | 'A' | 'B' | 'C' | 'AB' | 'AC' | 'BA' | 'BC' | 'CA' | 'CB' | 'ABC' | 'ACB' | 'BAC' | 'BCA' | 'CAB' | 'CBA'
+  ```
+
+  > View on GitHub: https://tsch.js.org/4260
+*/
+
+/* _____________ Your Code Here _____________ */
+
+//풀이 1 - ToUnion 타입과, Exclude 사용함
+
+//문자열을 유니온으로 만들어주는 type
+type ToUnion<S> = S extends `${infer F}${infer R}` ? F | ToUnion<R> : never;
+
+type AllCombinations<STR extends string, S extends string = ToUnion<STR>> = [
+  S
+] extends [never]
+  ? ""
+  : "" | { [K in S]: `${K}${AllCombinations<never, Exclude<S, K>>}` }[S];
+
+//풀이2 - 좀 더 풀어서 쓴듯
+type AllCombinations<
+  S,
+  U extends string = ""
+> = S extends `${infer SF}${infer SR}`
+  ? U extends `${infer UF}${infer UR}`
+    ? AllCombinations<
+        SR,
+        | U
+        | `${SF}${UR}${UF}`
+        | `${UF}${SF}${UR}`
+        | `${UR}${SF}${UF}`
+        | `${UR}${UF}${SF}`
+      >
+    : AllCombinations<SR, U | `${SF}${U}` | `${U}${SF}`>
+  : U;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<AllCombinations<"">, "">>,
+  Expect<Equal<AllCombinations<"A">, "" | "A">>,
+  Expect<Equal<AllCombinations<"AB">, "" | "A" | "B" | "AB" | "BA">>,
+  Expect<
+    Equal<
+      AllCombinations<"ABC">,
+      | ""
+      | "A"
+      | "B"
+      | "C"
+      | "AB"
+      | "AC"
+      | "BA"
+      | "BC"
+      | "CA"
+      | "CB"
+      | "ABC"
+      | "ACB"
+      | "BAC"
+      | "BCA"
+      | "CAB"
+      | "CBA"
+    >
+  >,
+  Expect<
+    Equal<
+      AllCombinations<"ABCD">,
+      | ""
+      | "A"
+      | "B"
+      | "C"
+      | "D"
+      | "AB"
+      | "AC"
+      | "AD"
+      | "BA"
+      | "BC"
+      | "BD"
+      | "CA"
+      | "CB"
+      | "CD"
+      | "DA"
+      | "DB"
+      | "DC"
+      | "ABC"
+      | "ABD"
+      | "ACB"
+      | "ACD"
+      | "ADB"
+      | "ADC"
+      | "BAC"
+      | "BAD"
+      | "BCA"
+      | "BCD"
+      | "BDA"
+      | "BDC"
+      | "CAB"
+      | "CAD"
+      | "CBA"
+      | "CBD"
+      | "CDA"
+      | "CDB"
+      | "DAB"
+      | "DAC"
+      | "DBA"
+      | "DBC"
+      | "DCA"
+      | "DCB"
+      | "ABCD"
+      | "ABDC"
+      | "ACBD"
+      | "ACDB"
+      | "ADBC"
+      | "ADCB"
+      | "BACD"
+      | "BADC"
+      | "BCAD"
+      | "BCDA"
+      | "BDAC"
+      | "BDCA"
+      | "CABD"
+      | "CADB"
+      | "CBAD"
+      | "CBDA"
+      | "CDAB"
+      | "CDBA"
+      | "DABC"
+      | "DACB"
+      | "DBAC"
+      | "DBCA"
+      | "DCAB"
+      | "DCBA"
+    >
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/4260/answer
+  > View solutions: https://tsch.js.org/4260/solutions
+  > More Challenges: https://tsch.js.org
+*/
